a30b5c3d8dcfbb06b79505ab4dfc1e6d4d9be6f5
Patrick Campanale <patrickappcreator@gmail.com>
2015-03-19 19:50:21 -0700

    carbon bringup
    Change-Id: Idb18e00c92018ee3046771f5198c587876c9236c

---
+++ carbon_hltetmo.mk
@@ -0,0 +1,11 @@
+$(call inherit-product, device/samsung/hltetmo/full_hltetmo.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/carbon/config/common.mk)
+
+PRODUCT_DEVICE := hltetmo
+PRODUCT_NAME := carbon_hltetmo
+PRODUCT_DEVICE := hltetmo
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := Galaxy Note 3
+PRODUCT_MANUFACTURE := Samsung
